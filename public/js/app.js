2d59a03fc25f6eabfd5a266ed0e0de5dabc9c514
Erik Griffin <griffin.erik@gmail.com>
2015-06-17 20:42:12 +0200

    Simple Sign Up Form

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,36 @@
 
   app.config(function($stateProvider,$urlRouterProvider,$locationProvider) {
 
-    // $stateProvider
-    //   .state('stateName', {
-    //     url:'/',
-    //     controller:'Controller as ctrl',
-    //     templateUrl: '/views/partials/template.html'
-    //   });
+    $stateProvider
+      .state('signUp', {
+        url:'/',
+        controller:'SignUpController as signUpCtrl',
+        templateUrl: '/views/partials/signup.html'
+      });
 
     $urlRouterProvider.otherwise('/');
 
     $locationProvider.html5Mode(true);
   });
+
+  app.controller('SignUpController', ['$http', function($http) {
+
+    var self = this;
+
+    self.signUp = function() {
+
+      var patient = {
+        email: self.patientEmail,
+        password: self.patientPassword,
+        confirmation: self.patientPasswordConfirmation
+      };
+
+      console.log(patient);
+
+    };
+
+
+  }]);
 
 
 }());
