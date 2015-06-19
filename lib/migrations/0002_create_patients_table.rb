1e0abbb79677fc2083668bd1220e74997714228f
Erik Griffin <griffin.erik@gmail.com>
2015-06-19 16:33:55 +0200

    Added note

--- lib/migrations/0002_create_patients_table.rb
+++ lib/migrations/0002_create_patients_table.rb
@@ -3,7 +3,6 @@ require 'sequel'
 Sequel.migration do
   up do
     create_table(:patients) do
-      # foreign_key :user_id, :users, primary_key: true
       foreign_key :id, :users, primary_key: true
       String :first_name, size: 30
       String :last_name, size: 30
